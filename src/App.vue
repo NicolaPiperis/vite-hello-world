6e61e93f79b3a4cd669fad7c338f7a5bf2ca0eab
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-05-30 16:10:54 +0200

    new component called newTitle

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import newTitle from './components/newTitle.vue';
+export default {
+  components: {
+    newTitle
+  }
+}
 </script>
 
 <template>
@@ -11,7 +16,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <newTitle/>
 </template>
 
 <style scoped>
---
+++ src/components/newTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name : "newTitle"
+}
+
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1{
+    font-size: 30px;
+    font-weight: 600;
+    color: brown;
+}
+</style>
